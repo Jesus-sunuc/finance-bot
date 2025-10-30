0c6d28f46446780ef3fe9f270cbe9574437f89bb
Jesus Sunuc <mariano.code4guate@gmail.com>
2025-10-29 20:33:49 -0600

    authentication added

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,18 +8,46 @@ import { getQueryClient } from "./services/queryClient.tsx";
 import { ThemeProvider } from "./components/theme/ThemeProvider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { LoadingAndErrorHandling } from "./components/LoadingAndErrorHandling.tsx";
+import { AuthProvider, type AuthProviderProps } from "react-oidc-context";
 
 const queryClient = getQueryClient();
+
+const oidcConfig: AuthProviderProps = {
+  authority: "https://auth-dev.snowse.io/realms/DevRealm",
+  client_id: "jesus-chat",
+  redirect_uri: window.location.origin + "/",
+  post_logout_redirect_uri: window.location.origin + "/",
+
+  onSigninCallback(user) {
+    console.log("User signed in:", user);
+    const url = new URL(window.location.href);
+    url.searchParams.delete("code");
+    url.searchParams.delete("state");
+    url.searchParams.delete("session_state");
+    url.searchParams.delete("iss");
+    window.history.replaceState(null, " ", url.toString());
+  },
+  onSignoutCallback() {
+    console.log("User signed out");
+  },
+
+  automaticSilentRenew: true,
+  loadUserInfo: true,
+
+  scope: "openid profile email",
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LoadingAndErrorHandling>
       <QueryClientProvider client={queryClient}>
+        <AuthProvider {...oidcConfig}>
         <ThemeProvider>
           <BrowserRouter>
             <App />
           </BrowserRouter>
         </ThemeProvider>
+        </AuthProvider>
       </QueryClientProvider>
     </LoadingAndErrorHandling>
   </StrictMode>
--- client/src/utils/axiosClient.ts
+++ client/src/utils/axiosClient.ts
@@ -4,6 +4,13 @@ import { handleDates } from "./handleDates";
 
 const axiosClient = axios.create();
 
+// Access token getter function
+let getAccessToken: (() => string | undefined) | null = null;
+
+export const setAccessTokenGetter = (getter: () => string | undefined) => {
+  getAccessToken = getter;
+};
+
 axiosClient.interceptors.response.use((originalResponse) => {
   originalResponse.data = snakeToCamel(originalResponse.data);
   return originalResponse;
@@ -15,6 +22,14 @@ axiosClient.interceptors.response.use((originalResponse) => {
 });
 
 axiosClient.interceptors.request.use((config) => {
+  // Add Authorization header if token is available
+  if (getAccessToken) {
+    const token = getAccessToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
   if (config.data instanceof FormData) return config;
   config.data = camel_to_snake_serializing_date(config.data);
   return config;
